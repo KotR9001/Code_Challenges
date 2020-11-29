eda5916966f4c4d516a8e447af051b7740166624
KotR9001 <57807780+KotR9001@users.noreply.github.com>
2020-11-29 11:04:36 -0600

    Update SQL challenge.

--- Hacker Rank/SQL/Weather_Observations.sql
+++ Hacker Rank/SQL/Weather_Observations.sql
@@ -29,3 +29,7 @@ from station
 group by city
 order by length(city) desc, city asc
 limit 1);
+
+select distinct city
+from station
+where city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%';
